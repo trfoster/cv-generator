bb4f3fc47b155f341218e334c36d6376fc3ac5ae
Tom Foster <trfoster1794@gmail.com>
2025-11-11 00:41:14 +0000

    Made initial page

--- PDFGenerator/Program.cs
+++ PDFGenerator/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Write("PDFS!");
+﻿using QuestPDF.Companion;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using Document = QuestPDF.Fluent.Document;
+
+QuestPDF.Settings.License = LicenseType.Community;
+
+Document.Create(container => {
+    container.Page(page => {
+        page.Size(PageSizes.A4);
+        page.Margin(2, Unit.Centimetre);
+        page.DefaultTextStyle(t => t.FontSize(12).FontFamily("Courier"));
+        
+        page.Header().Text("Tom Foster").Bold().FontSize(30).FontFamily("Courier").AlignCenter();
+
+        page.Content().PaddingVertical(0.5f, Unit.Centimetre).Column(c => {
+            c.Item().Text("This is a really amazing CV");
+            c.Item().Image(Placeholders.Image(100, 100));
+        });
+
+        page.Footer().AlignCenter().Text("Really nice footer");
+    });
+}).ShowInCompanion();
